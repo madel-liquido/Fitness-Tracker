5fd4138af47ffdade05b69beda44845e8f2b95e0
Madel Liquido <madel.liquido@gmail.com>
2020-04-23 18:10:06 -0400

    Need to edit the api routes. Getting a validation error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,36 @@
-//need to add get, post, put requests
+const db = require("../models/workout.js")
+const mongojs = require("mongojs");
+const express = require("express");
+
+module.exports = function(app) {
+
+    app.get("/api/workouts", function(req, res) {
+        db.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    });
+
+    app.post("/api/workouts", function(req, res) {
+        db.create({})
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
+    });
+
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        db.findByIdAndUpdate(
+                params.id, { $push: { exercises: body } }, { new: true, runValidators: true }
+            )
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
+    });
+}
