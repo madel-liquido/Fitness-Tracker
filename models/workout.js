614e63200b74ae12dd9c588cd9edd92042662f5d
Madel Liquido <madel.liquido@gmail.com>
2020-04-23 17:56:55 -0400

    Added schema to workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,51 @@
 //need to add workout schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter the exercise type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter the exercise name"
+        },
+        duration: {
+            type: Number,
+            required: "Enter the duration of the exercise in minutes"
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+workoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
